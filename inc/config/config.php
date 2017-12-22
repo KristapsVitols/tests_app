5c86ce154493890c5232c4acd061d94f33d5422f
KristapsVitols <vitolskristaps@inbox.lv>
2017-12-22 12:25:41 +0200

    fix

--- inc/config/config.php
+++ inc/config/config.php
@@ -1,11 +1,6 @@
 <?php
 // DB INFO
-define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'); // hostname
-define('DB_USER', 'b037b7c9338955'); // user
-define('DB_PASS', 'f12d26cc'); // password
-define('DB_NAME', 'heroku_10d389039b33105'); // database name
-
-// define('DB_HOST', 'localhost'); // hostname
-// define('DB_USER', 'root'); // user
-// define('DB_PASS', 'password'); // password
-// define('DB_NAME', 'printfulapp'); // database name
+define('DB_HOST', 'localhost'); // hostname
+define('DB_USER', 'root'); // user
+define('DB_PASS', 'password'); // password
+define('DB_NAME', 'printfulapp'); // database name
